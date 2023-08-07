1bd4524761cf500d4a3f0d4525d14acde3e44756
phorgisonthejeep <131064009+phorgisonthejeep@users.noreply.github.com>
2023-08-07 22:15:23 +0000

    asd

--- counter.php
+++
@@ -1,6 +0,0 @@
-<?php
-$file = "count.txt"; // Path to the text file that stores the count
-$count = file_get_contents($file); // Get the current count from the file
-$count++; // Increment the count
-file_put_contents($file, $count); // Save the new count to the file
-?>
